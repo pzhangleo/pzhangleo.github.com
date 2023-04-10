5e543a4463c116037fa17a19632e404ff71bf0cd
pzhangleo <pengzh.leo@gmail.com>
2023-04-10 21:58:29 +0800

    change request internal

--- src/component/BitcoinPriceView.tsx
+++ src/component/BitcoinPriceView.tsx
@@ -5,34 +5,31 @@ interface BitcoinPriceProps {
 }
 
 interface BitcoinPrice {
-  last: number;
-  high: number;
-  low: number;
-  volume: number;
+  USD: String;
 }
 
-const BitcoinPrice: React.FC<BitcoinPriceProps> = ({ currency }) => {
+const BitcoinPriceView: React.FC<BitcoinPriceProps> = ({ currency }) => {
   const [price, setPrice] = useState<BitcoinPrice | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
-        `https://www.bitstamp.net/api/v2/ticker/btc${currency}/`,
-        //add Origin header to avoid CORS error
+        `https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=${currency}&api_key=fc7af115e9a270c513c311c0b92e10337d9d93eff3c7e245efdef79fec6f1341`,
         {
-            mode: 'cors',
             headers: {
               'Content-Type': 'application/json',
               'Access-Control-Allow-Origin': '*'
             }
           }
       );
+      console.log(response);
+      if(!response.ok) throw new Error(response.statusText);
       const data = await response.json();
       setPrice(data);
     };
     fetchData();
     //change the interval to 5 minutes  5 * 60 * 1000
-    const interval = setInterval(fetchData, 5 * 60 * 1000);
+    const interval = setInterval(fetchData, 5000);
     return () => clearInterval(interval);
   }, [currency]);
 
@@ -44,19 +41,11 @@ const BitcoinPrice: React.FC<BitcoinPriceProps> = ({ currency }) => {
     <div className="border-2 border-gray-300 p-4 rounded-lg">
       <h2 className="text-xl font-medium mb-4">Bitcoin Price</h2>
       <p className="text-lg mb-2">
-        Last Price: {price.last.toFixed(2)} {currency.toUpperCase()}
+        Price: ${price.USD} {currency.toUpperCase()}
       </p>
-      <p className="text-lg mb-2">
-        High Price: {price.high.toFixed(2)} {currency.toUpperCase()}
-      </p>
-      <p className="text-lg mb-2">
-        Low Price: {price.low.toFixed(2)} {currency.toUpperCase()}
-      </p>
-      <p className="text-lg mb-2">
-        Volume: {price.volume.toFixed(2)} BTC
-      </p>
+
     </div>
   );
 };
 
-export default BitcoinPrice;
+export default BitcoinPriceView;
